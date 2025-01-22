3c72e7426a0a737eb57020ccc22bada669d178e9
rifrifqyqy <rifqyh22@gmail.com>
2025-01-22 11:36:59 +0700

    feat: add button for testing (sure)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import motokoLogo from './assets/motoko_moving.png';
 import motokoShadowLogo from './assets/motoko_shadow.png';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
+import Button from './components/elements/Button';
 
 function App() {
   const { data: count, refetch } = useQueryCall({
@@ -18,6 +19,7 @@ function App() {
   return (
     <div className="App bg-slate-300">
      Create Home
+     <Button>Button Test</Button>
     </div>
   );
 }
