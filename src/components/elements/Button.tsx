ade112df3685c312fedac96fba718f40b162d18e
rifrifqyqy <rifqyh22@gmail.com>
2025-01-22 21:32:44 +0700

    feat: create button secondary theme

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
   children: React.ReactNode
   onclick?: () => void
 }
-
+let _transition = "transition-all duration-300";
 export function Button({ children, onclick }: ButtonProps) {
   return (
     <button
@@ -19,7 +19,20 @@ export function ButtonMagenta({ children, onclick }: ButtonProps) {
   return (
     <button
       onClick={onclick}
-      className="bg-magentas-100 text-white font-semibold py-2 px-4 rounded-md hover:opacity-80 transition-all"
+      className={`bg-magentas-100 text-white font-semibold py-2 px-4 rounded-md hover:opacity-80 ${_transition}`}
+    >
+      <span>
+        {children}
+      </span>
+    </button>
+  )
+}
+
+export function ButtonSecondary({ children, onclick }: ButtonProps) {
+  return (
+    <button
+      onClick={onclick}
+      className={`text-dark-200 font-semibold py-2 px-4 rounded-md border border-transparent hover:border-magentas-100 hover:text-magentas-100 transition-all duration-300 ${_transition}`}
     >
       <span>
         {children}
