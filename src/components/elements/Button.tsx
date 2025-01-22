2460de350f45b8e4e2addc6675fdc7cbc5366ee6
rifrifqyqy <rifqyh22@gmail.com>
2025-01-22 21:09:04 +0700

    feat: add button magenta styles

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -5,10 +5,25 @@ interface ButtonProps {
   onclick?: () => void
 }
 
-export function Button ({children, onclick}: ButtonProps) {
+export function Button({ children, onclick }: ButtonProps) {
   return (
-    <button onClick={onclick} className=" text-white font-semibold py-2 px-4 rounded-md btn-gradient">
-    <span>{children}</span>
+    <button
+      onClick={onclick}
+      className="text-white font-semibold py-2 px-4 rounded-md btn-gradient">
+      <span>{children}</span>
+    </button>
+  )
+}
+
+export function ButtonMagenta({ children, onclick }: ButtonProps) {
+  return (
+    <button
+      onClick={onclick}
+      className="bg-magentas-100 text-white font-semibold py-2 px-4 rounded-md hover:opacity-80 transition-all"
+    >
+      <span>
+        {children}
+      </span>
     </button>
   )
 }
