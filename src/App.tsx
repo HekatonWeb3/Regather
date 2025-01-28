22e898d6e89d3a4e62e0595e45c4514c2743270e
rifrifqyqy <rifqyh22@gmail.com>
2025-01-28 20:09:13 +0700

    refactor: remove route logic to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
 import { useQueryCall, useUpdateCall } from '@ic-reactor/react';
 import './App.css';
-import {Button} from './components/elements/Button';
+import { Button } from './components/elements/Button';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Layouts from './components/layouts/Layouts';
+import LandingPage from './pages/LandingPage';
+import HomePage from './pages/Homepage';
+import AuthLogin from './pages/AuthLogin';
+import { useAuth } from './hooks/useAuth';
+import ProtectedRoutex from './pages/protected/ProtectedRoutex';
+import CreateNovel from './pages/protected/CreateNovel';
 
 function App() {
-  const { data: count, refetch } = useQueryCall({
-    functionName: 'get',
-  });
-
-  const { call: increment, loading } = useUpdateCall({
-    functionName: 'inc',
-    onSuccess: refetch,
-  });
-
-  return (
-    <div className="App bg-slate-300">
-     Create Home
-     <Button>Button Test</Button>
-    </div>
-  );
+  const { isAuthenticated, loading } = useAuth();
+  const router = createBrowserRouter([{
+    element: <Layouts />,
+    children: [
+      {
+        path: '/',
+        element: isAuthenticated ? <HomePage /> : <LandingPage />,
+      },
+      {
+        path: "/login",
+        element: <AuthLogin />
+      },
+      {
+        path: "/create",
+        element: (
+          <ProtectedRoutex bypass={isAuthenticated}>
+            <CreateNovel />
+          </ProtectedRoutex>
+        ),
+        ErrorBoundary: () => {
+          return <div>Something went wrong</div>
+        }
+      }
+    ]
+  }])
+  if (loading) {
+    return <div>Loading...</div>; // Menunggu pengecekan autentikasi selesai
+  }
+  return <RouterProvider router={router} />
 }
 
 export default App;
